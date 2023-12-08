05a03196ed65d2702b2c10ea15f0b956e675c713
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-12-08 14:26:02 +0100

    Test parsing file scheme with root path

---
+++ test/parse-file-scheme-root-path.c
@@ -0,0 +1,16 @@
+#include "helpers.h"
+
+int
+main () {
+  test_parse(url, "file:///", NULL);
+
+  test_component(url, href, "file:///");
+  test_component(url, scheme, "file");
+  test_component(url, username, "");
+  test_component(url, password, "");
+  test_component(url, host, "");
+  test_component(url, port, "");
+  test_component(url, path, "/");
+  test_component(url, query, "");
+  test_component(url, fragment, "");
+}
