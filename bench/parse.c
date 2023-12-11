0220eae602f2fdcf6028b307d7baba8be03ceb56
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-12-11 19:39:11 +0100

    Add simple benchmark

---
+++ bench/parse.c
@@ -0,0 +1,11 @@
+#include "../include/url.h"
+
+int
+main () {
+  url_t url;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    url_parse(&url, (utf8_t *) "https://example.com/hello/world?query=string#fragment", -1, NULL);
+    url_destroy(&url);
+  }
+}
