07df218ca2e2905c65c000ae640d20a44b0cbdc1
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-12-13 11:19:48 +0100

    Improve ASCII case transform

--- src/infra.h
+++ src/infra.h
@@ -37,13 +37,13 @@ url__is_ascii_alphanumeric (utf8_t c) {
 // https://infra.spec.whatwg.org/#ascii-lowercase
 static inline utf8_t
 url__to_ascii_lowercase (utf8_t c) {
-  return url__is_ascii_upper_alpha(c) ? c ^ 0x20 : c;
+  return c | 0x20;
 }
 
 // https://infra.spec.whatwg.org/#ascii-uppercase
 static inline utf8_t
 url__to_ascii_uppercase (utf8_t c) {
-  return url__is_ascii_lower_alpha(c) ? c ^ 0x20 : c;
+  return c & ~0x20;
 }
 
 #endif // URL_INFRA_H
