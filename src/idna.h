711895fe10c336603bfb75103e742168ec00912b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-12-14 11:57:34 +0100

    Add IDNA and Punycode placeholders

---
+++ src/idna.h
@@ -0,0 +1,14 @@
+#ifndef URL_IDNA_H
+#define URL_IDNA_H
+
+#include <utf.h>
+#include <utf/string.h>
+
+static inline int
+url__idna_to_ascii (utf8_string_view_t input, utf8_string_t *result) {
+  // TODO
+
+  return -1;
+}
+
+#endif // URL_IDNA_H
---
+++ src/punycode.h
@@ -0,0 +1,21 @@
+#ifndef URL_PUNYCODE_H
+#define URL_PUNYCODE_H
+
+#include <utf.h>
+#include <utf/string.h>
+
+static inline int
+url__punycode_to_utf8 (utf8_string_view_t input, utf8_string_t *result) {
+  // TODO
+
+  return -1;
+}
+
+static inline int
+url__utf8_to_punycode (utf8_string_view_t input, utf8_string_t *result) {
+  // TODO
+
+  return -1;
+}
+
+#endif // URL_PUNYCODE_H
